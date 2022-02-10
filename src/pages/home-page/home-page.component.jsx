cde8868a30b51b064dc062e1e5b84924291e5e09
Faraz Khalid <farazkhalid05@yahoo.com>
2022-02-11 02:56:14 +0500

    search filter completed

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
 import Post from '../../components/post/post.component';
 import Header from '../../components/header/header';
@@ -6,22 +6,15 @@ import Header from '../../components/header/header';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
+
 
 import './home-page.styles.css'
 
 const Home = ({posts}) => {
 
-  const valueRef = React.useRef('') //creating a refernce for TextField Component
-
-  const sendValue = () => {
-      console.log(posts)
-      posts = posts.filter(post => post.title.includes(valueRef.current.value))
-  }
+  const [value, _value] = useState('')
 
   return(
-    
     <Container component="main" maxWidth="lg" >
      <Header title='home'/>
      
@@ -29,17 +22,21 @@ const Home = ({posts}) => {
           sx={{ marginBottom:'30px'}}
           placeholder="Type to Search"
           size="small"
-          inputRef={valueRef}
-          onKeyDown={sendValue}
+          onChange={(e) => _value(e.target.value)}
           type="search"
+          value={value}
       />
-      {/* <IconButton type="submit" aria-label="search" onClick={sendValue}>
-        <SearchIcon />
-      </IconButton> */}
       
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 1, md: 8 }}>
             {
-              posts.map((post) =>  (
+              value === '' && posts.map((post) =>  (
+                <Grid item xs={2} sm={4} md={4} >
+                  <Post id={post.id} key={post.id} title={post.title} body={post.body}/>
+                </Grid>
+              ))
+            }
+			{
+              value !== '' && posts.filter(x => x.title.includes(value)).map((post) =>  (
                 <Grid item xs={2} sm={4} md={4} >
                   <Post id={post.id} key={post.id} title={post.title} body={post.body}/>
                 </Grid>
